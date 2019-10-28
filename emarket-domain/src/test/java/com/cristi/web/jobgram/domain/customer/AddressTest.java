3cb9b2c9c6ba16421970dea5c808d85c5a662973
Cristi <cristi_zmf@yahoo.com>
2019-10-28 23:04:39 +0200

    Address simple VO

---
+++ emarket-domain/src/test/java/com/cristi/web/jobgram/domain/customer/AddressTest.java
@@ -0,0 +1,23 @@
+package com.cristi.web.jobgram.domain.customer;
+
+import com.cristi.web.jobgram.domain.ddd.DomainConstraintViolationException;
+import org.junit.Test;
+
+public class AddressTest {
+
+    @Test(expected = DomainConstraintViolationException.class)
+    public void when_streetName_is_empty_then_constraint_exception() {
+        new Address("", 32);
+    }
+
+    @Test(expected = DomainConstraintViolationException.class)
+    public void when_streetName_is_blank_then_constraint_exception() {
+        new Address("     ", 41);
+    }
+
+    @Test(expected = DomainConstraintViolationException.class)
+    public void when_streetName_is_null_then_constraint_exception() {
+        new Address(null, 41);
+    }
+
+}
