c21f5ee780ea2d0e048332927dee7d3cdd6452eb
Cristi <cristi_zmf@yahoo.com>
2019-11-15 08:56:02 +0200

    Remove orders from customers aggregate

--- emarket-domain/src/main/java/com/cristi/web/emarket/domain/customer/Customer.java
+++ emarket-domain/src/main/java/com/cristi/web/emarket/domain/customer/Customer.java
@@ -4,15 +4,11 @@ import com.cristi.web.emarket.domain.Address;
 import com.cristi.web.emarket.domain.UniqueId;
 import com.cristi.web.emarket.domain.ddd.BaseAggregateRoot;
 import com.cristi.web.emarket.domain.ddd.DDD;
-import com.cristi.web.emarket.domain.order.Order;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
-import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
-
-import static java.util.Collections.unmodifiableSet;
 
 @DDD.AggregateRoot
 public class Customer extends BaseAggregateRoot<Customer, UniqueId> {
@@ -21,24 +17,17 @@ public class Customer extends BaseAggregateRoot<Customer, UniqueId> {
     @NotNull
     private Address address;
     private CreditCard creditCard;
-    @NotNull
-    private Set<UniqueId> orders;
 
     Customer(UniqueId uniqueId, CustomerName name, Address address, CreditCard creditCard, Set<UniqueId> orders) {
         super(Customer.class, uniqueId);
         this.name = name;
         this.address = address;
         this.creditCard = creditCard;
-        this.orders = new HashSet<>(orders);
         validate(this);
     }
 
     public Customer(CustomerName customerName, @NotNull Address address, @NotEmpty Set<UniqueId> orders) {
         this(new UniqueId(), customerName, address, null, orders);
-    }
-
-    public Set<UniqueId> getOrders() {
-        return unmodifiableSet(orders);
     }
 
     public NamePart getFirstName() {
@@ -62,9 +51,5 @@ public class Customer extends BaseAggregateRoot<Customer, UniqueId> {
         super(Customer.class);
         address = null;
         creditCard = null;
-    }
-
-    public void placeNewOrder(UniqueId newOrder) {
-        orders.add(newOrder);
     }
 }
